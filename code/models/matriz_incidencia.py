8ebe7597f7b47d7fc241f9e08afe16ead628910a
Victor Reis Carlota <128194246+victorreiscarlota@users.noreply.github.com>
2024-11-29 23:12:38 -0300

    correção da lista de adjacencia

---
+++ code/models/matriz_incidencia.py
@@ -0,0 +1,45 @@
+# grafo_lib/representacoes/matriz_incidencia.py
+
+class MatrizIncidencia:
+    def __init__(self, num_vertices, dirigido=False):
+        self.num_vertices = num_vertices
+        self.dirigido = dirigido
+        self.inc_matrix = []  
+        self.edge_list = []   
+
+    def adicionar_aresta(self, u, v, peso=1, label=None):
+        self.edge_list.append({'u': u, 'v': v, 'peso': peso, 'label': label})
+        edge_index = len(self.edge_list) - 1
+
+        for row in self.inc_matrix:
+            row.append(0)
+        
+        nova_coluna = [0] * self.num_vertices
+        nova_coluna[u] = 1
+        if not self.dirigido:
+            nova_coluna[v] = 1
+        else:
+            nova_coluna[v] = -1
+        self.inc_matrix.append(nova_coluna)
+
+    def remover_aresta(self, u, v):
+        for i, edge in enumerate(self.edge_list):
+            if edge['u'] == u and edge['v'] == v:
+                del self.edge_list[i]
+                del self.inc_matrix[i]
+                for row in self.inc_matrix:
+                    del row[i]
+                break
+
+    def checar_adjacencia(self, u, v):
+        for edge in self.edge_list:
+            if edge['u'] == u and edge['v'] == v:
+                return True
+            if not self.dirigido and edge['u'] == v and edge['v'] == u:
+                return True
+        return False
+
+    def exibir(self):
+        print("Matriz de Incidência:")
+        for row in self.inc_matrix:
+            print(row)
---
+++ code/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HaraHetta',
+    version='1.0.0',
+    description='Uma biblioteca para manipulação de Grafos feita em python, aproveitem para contribuir!',
+    author=[
+        'Gustavo Pereira de Oliveira',
+        'Luís Felipe Teixeira Dias Brescia',
+        'Luiz Fernando Campos de Morais',
+        'Marcus Vinícius de Oliveira',
+        'Victor Reis Carlota',
+    ],
+    author_email=[
+        'gustavo.oliveira.1403216@sga.pucminas.br',
+        'luisfelipebrescia@gmail.com',
+        'lfcamposdemorais@gmail.com,',
+        'marcus.oliveira.1320200@sga.pucminas.br',
+        'victor.reis.carlotta@gmail.com',
+    ],
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3.11.4',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
